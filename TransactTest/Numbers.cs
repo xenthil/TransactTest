ee71a9e9e8a634e5a2fe0312250d217e0903d8b6
Senthilkumar Subramanian <Senthilkumar.Subra@gisbizdev.gisbiz.chennai.com>
2020-02-19 15:06:58 +0530

    Initial commit

---
+++ TransactTest/Numbers.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace TransactTest
+{
+    public class Numbers
+    {
+        int _low;
+        int _high;
+        int _a;
+        int _b;
+
+        public Numbers(int low, int high, int a, int b)
+        {
+            _low = low;
+            _high = high;
+            _a = a;
+            _b = b;
+        }        
+
+        public List<string> GetOutput()
+        {
+            string result;
+            var output = new List<string>();
+
+            for (int number = _low; number <= _high; number++)
+            {
+                result = string.Empty;
+
+                if (number % _a == 0)
+                {
+                    result = "Fancy";
+                }
+                if (number % _b == 0)
+                {
+                    result += "Pants";
+                }
+
+                output.Add(string.IsNullOrEmpty(result) ? number.ToString() : result);
+            }
+
+            return output;
+        }
+    }
+}
---
+++ TransactTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TransactTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int low;
+            int high;
+            int a;
+            int b;
+
+            GetIntegerInput(out low);
+            GetIntegerInput(out high);
+            GetIntegerInput(out a);
+            GetIntegerInput(out b);
+
+            var numbers = new Numbers(low, high, a, b);
+            var output = numbers.GetOutput();
+
+            foreach (var item in output)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void GetIntegerInput(out int number)
+        {
+            Console.WriteLine("Enter a positive number: ");
+            string input = Console.ReadLine();
+
+            if (!int.TryParse(input, out number) || number < 0)
+            {
+                Console.WriteLine("Invalid input");
+                GetIntegerInput(out number);
+            }
+        }
+    }
+}
